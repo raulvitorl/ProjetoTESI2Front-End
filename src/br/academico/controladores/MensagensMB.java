778301581955acccd863bc7bd3f673c7d34a239c
Raul Vitor L <raulawliet@gmail.com>
2018-12-04 13:27:27 -0500

    Mensagens

--- src/br/academico/controladores/MensagensMB.java
+++ src/br/academico/controladores/MensagensMB.java
@@ -10,17 +10,37 @@ import br.ufac.academico.repositorios.*;
 @ManagedBean(name="mensagemControlador")
 @SessionScoped
 public class MensagensMB { 
-	private MensagensRepositorio br;
+	private MensagensRepositorio mr;
+	private AtendentesRepositorio ar;
+	private TiposMensagensRepositorio tmsr;
 	private List<Mensagens> mensagens;
 	private String chaveNome = "";
 	private Mensagens mensagem;
 	private long men_codigo;
-	private Atendentes men_ate_codigo;
-	private TiposMensagens men_tms_codigo;
+	private long men_ate_codigo;
+	private long men_tms_codigo;
 	private String men_texto;
 	private String men_data_envio;
+
 	
+
+	public void setMen_ate_codigo(long men_ate_codigo) {
+		this.men_ate_codigo = men_ate_codigo;
+	}
+
+	public void setMen_tms_codigo(long men_tms_codigo) {
+		this.men_tms_codigo = men_tms_codigo;
+	}
+
+	public MensagensMB() {
+		mensagem = new Mensagens();
+		mr = new MensagensRepositorio();
+		ar = new AtendentesRepositorio();
+		tmsr = new TiposMensagensRepositorio();
+	}
+
 	public List<Mensagens> getMensagens() {
+		mensagens = mr.recuperarTodos();
 		return mensagens;
 	}
 	public void setMensagens(List<Mensagens> mensagens) {
@@ -44,16 +64,16 @@ public class MensagensMB {
 	public void setMen_codigo(long men_codigo) {
 		this.men_codigo = men_codigo;
 	}
-	public Atendentes getMen_ate_codigo() {
+	public long getMen_ate_codigo() {
 		return men_ate_codigo;
 	}
-	public void setMen_ate_codigo(Atendentes men_ate_codigo) {
+	public void setMen_ate_codigo(int men_ate_codigo) {
 		this.men_ate_codigo = men_ate_codigo;
 	}
-	public TiposMensagens getMen_tms_codigo() {
+	public long getMen_tms_codigo() {
 		return men_tms_codigo;
 	}
-	public void setMen_tms_codigo(TiposMensagens men_tms_codigo) {
+	public void setMen_tms_codigo(int men_tms_codigo) {
 		this.men_tms_codigo = men_tms_codigo;
 	}
 	public String getMen_texto() {
@@ -69,32 +89,41 @@ public class MensagensMB {
 		this.men_data_envio = men_data_envio;
 	}
 	public String incluir() {
-		br.adicionar(mensagem);
-		return "mensagemListagem";
-	}
-	public String editar(Mensagens mensagem) {
-		this.mensagem=mensagem;
-		return "mensagemEdicao";
-	}
-	public String novo() {
-		mensagem = new Mensagens();
+		mensagem = new Mensagens();		
 		return "mensagemInclusao";
 	}
-	public String alterar() {
-		br.atualizar(mensagem);
+
+	public String adicionar(){
+		mensagem.setMen_ate_codigo(ar.recuperar(men_ate_codigo));
+		mensagem.setTipoMensagem(tmsr.recuperar(men_tms_codigo));
+		mr.adicionar(mensagem);
 		return "mensagemListagem";
 	}
-	public String remover() {
-		br.remover(mensagem);
+
+	public String editar(Mensagens mensagem) {
+		this.mensagem=mensagem;
+		men_tms_codigo = mensagem.getTipoMensagem().getTmsCodigo();
+		men_ate_codigo = mensagem.getMen_ate_codigo().getCodigo();
+		return "mensagemEdicao";
+	}
+
+	public String atualizar() {
+		mensagem.setMen_ate_codigo(ar.recuperar(men_ate_codigo));
+		mensagem.setTipoMensagem(tmsr.recuperar(men_tms_codigo));
+		mr.atualizar(mensagem);
 		return "mensagemListagem";
 	}
+
 	public String excluir(Mensagens mensagem) {
 		this.mensagem=mensagem;
+		men_tms_codigo = mensagem.getTipoMensagem().getTmsCodigo();
+		men_ate_codigo = mensagem.getMen_ate_codigo().getCodigo();
 		return "mensagemExclusao";
 	}
-	public void setBanco(Mensagens mensagem) {
-		this.mensagem = mensagem;
+	
+	public String remover() {
+		mr.remover(mensagem);
+		return "mensagemListagem";
 	}
-	
-	
+
 }
