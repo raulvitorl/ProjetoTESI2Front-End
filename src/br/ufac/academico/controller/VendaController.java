e8139c91e8cce39124a9db495fca5065821a5755
Raul Vitor L <raulawliet@gmail.com>
2018-12-16 13:25:45 -0500

    Relatório de venda implementado

--- src/br/ufac/academico/controller/VendaController.java
+++ src/br/ufac/academico/controller/VendaController.java
@@ -1,8 +1,22 @@
 package br.ufac.academico.controller;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
 import javax.faces.bean.*;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import br.ufac.academico.domain.*;
 import br.ufac.academico.repositories.*;
 
@@ -23,9 +37,9 @@ public class VendaController {
 	private Integer proCodigo;
 	private List <Produto> produtos = new ArrayList<>();
 	private Produto produto;
-	
-	
-	
+
+
+
 	public Integer getProCodigo() {
 		return proCodigo;
 	}
@@ -61,9 +75,9 @@ public class VendaController {
 	public void setChaveNome(String chaveNome) {
 		this.chaveNome = chaveNome;
 	}	
-	
-	
-	
+
+
+
 	public Integer getCliCodigo() {
 		return cliCodigo;
 	}
@@ -88,7 +102,7 @@ public class VendaController {
 	public void setVenda(Venda venda) {
 		this.venda = venda;
 	}
-	
+
 	public Integer getBanCodigo() {
 		return banCodigo;
 	}
@@ -131,8 +145,8 @@ public class VendaController {
 		ateCodigo = venda.getAtendente().getCodigo();
 		return "vendaExclusao";
 	}
-	
-	
+
+
 	public String adiciona(){
 		produto = pr.recuperar(proCodigo);
 		produtos.add(produto);
@@ -142,7 +156,7 @@ public class VendaController {
 		System.out.println("Tem "+produto.getQntDisponivel()+" Sofás");
 		return "#";
 	}
-	
+
 	public float valorTotal(List <Produto> produtos){
 		if(produtos.isEmpty() || produtos==null)
 			return 0;
@@ -150,10 +164,64 @@ public class VendaController {
 		for(int i=0;i<produtos.size();i++){
 			valor+=produtos.get(i).getValorUnitario();
 		}
-		
+
 		return valor;
 	}
-	
+
+	public String relatorio(Venda v){ 
+		String nome = UUID.randomUUID().toString();
+		produtos = v.getProdutos(); 
+		Document document = new Document(); 
+		try { 
+			PdfWriter.getInstance(document, new FileOutputStream(nome+".pdf")); 
+			} catch (FileNotFoundException | DocumentException e) 
+		{ e.printStackTrace(); } 
+			document.open();
+			PdfPTable table = new PdfPTable(8);
+		try { 
+			document.add(new Paragraph("CLIENTE:  "+v.getCliente().getNome()));
+			document.add(new Paragraph("VENDEDOR:  "+v.getAtendente().getNome()));
+			document.add(new Paragraph("CÓDIGO  PRODUTO VALOR ")); 
+			for(Produto p: produtos) { 
+				document.add(new Paragraph("     "+p.getCodigo()+"     |     "+p.getDescricao()+"     |      "+p.getValorUnitario())); 
+				}
+			document.add(new Paragraph("VALOR TOTAL: R$"+v.getValorTotal()));
+			document.add(new Paragraph("FORMA DE PAGAMENTO: "+v.getFormaPagamento()));
+			}
+		
+		catch (DocumentException e) { e.printStackTrace(); }
+		try {
+			document.add(table);
+		} catch (DocumentException e1) {
+			e1.printStackTrace();
+		}
+		document.close(); 
+		try { 
+			Desktop.getDesktop().open(new File(nome+".pdf"));
+			} catch (IOException e) { e.printStackTrace(); } 
+		return "vendaListagem"; 
+		}
+
 
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
