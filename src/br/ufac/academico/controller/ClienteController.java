0d6d1213117c8e45fbf38c992bdef53cc00a008a
Raul Vitor L <raulawliet@gmail.com>
2018-12-16 15:36:27 -0500

    Melhorias na interface

--- src/br/ufac/academico/controller/ClienteController.java
+++ src/br/ufac/academico/controller/ClienteController.java
@@ -1,8 +1,21 @@
 package br.ufac.academico.controller;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.faces.bean.*;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import br.ufac.academico.domain.*;
 import br.ufac.academico.repositories.*;
@@ -13,6 +26,7 @@ public class ClienteController {
 	private ClienteRepositorio br;
 	private MunicipioRepositorio mr;
 	private List<Cliente> clientes;
+	private List <Venda> vendas = new ArrayList<>();
 	private Integer munCodigo;
 	private String chaveNome = "";
 	private Cliente cliente;
@@ -81,6 +95,44 @@ public class ClienteController {
 	public void setBanco(Cliente cliente) {
 		this.cliente = cliente;
 	}
+	
+	public String relatorio(Cliente c){ 
+		String nome = UUID.randomUUID().toString();
+		vendas = c.getVendas(); 
+		float valortotal = 0;
+		Document document = new Document(); 
+		try { 
+			PdfWriter.getInstance(document, new FileOutputStream(nome+".pdf")); 
+			} catch (FileNotFoundException | DocumentException e) 
+		{ e.printStackTrace(); } 
+			document.open();
+			PdfPTable table = new PdfPTable(8);
+		try { 
+			document.add(new Paragraph("CLIENTE:  "+c.getCodigo()+"   "+c.getNome()));
+			document.add(new Paragraph("CLIENTE DESDE:  "+c.getCadastro()));
+			document.add(new Paragraph("JÁ REALIZOU  :"+vendas.size()+" COMPRAS EM NOSSA LOJA"));
+			document.add(new Paragraph("----------------------COMPRAS--------------------------------"));
+			for(Venda v: vendas) { 
+				document.add(new Paragraph("        "+v.getCodigo()+"        |        "+v.getValorTotal()+"        |         "+v.getFormaPagamento()+"      |      "+v.getStatusPagamento())); 
+				valortotal+=v.getValorTotal();
+			}
+			document.add(new Paragraph("VALOR TOTAL GASTO: R$"+valortotal));
+			}
+		
+		catch (DocumentException e) { e.printStackTrace(); }
+		try {
+			document.add(table);
+		} catch (DocumentException e1) {
+			e1.printStackTrace();
+		}
+		document.close(); 
+		try { 
+			Desktop.getDesktop().open(new File(nome+".pdf"));
+			} catch (IOException e) { e.printStackTrace(); } 
+		return "vendaListagem"; 
+		}
+
+
 	
 	
 }
